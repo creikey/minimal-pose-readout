268e31f8e8a32740675d9a2020fe62873d9dc559
Omkar Bhope <omkarbhope0309@gmail.com>
2023-04-03 16:50:08 -0700

    Initial Commit

---
+++ Minimal Pose Readout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minimal Pose Readout
+//
+//  Created by Omkar Bhope on 4/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
